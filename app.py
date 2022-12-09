b9bc12848e6d6e37a257e1a55d216cbc453855db
Habeeb Alao <72545249+HabeebAlao@users.noreply.github.com>
2022-12-09 12:48:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'my-cloud-sql-instance'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
