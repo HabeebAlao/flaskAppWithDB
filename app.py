addcc8f1c4be8a0dbcfbff0a7ce5811f24f0b6f5
Habeeb Alao <72545249+HabeebAlao@users.noreply.github.com>
2022-12-09 18:40:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 CORS(app)
 # My SQL Instance configurations
 # Change these details to match your instance configurations
-app.config['MYSQL_USER'] = 'A'
-app.config['MYSQL_PASSWORD'] = 'B'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = '34.173.11.49'
 mysql.init_app(app)
